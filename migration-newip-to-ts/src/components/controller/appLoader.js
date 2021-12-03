49a395aee8c9f4bd8cbaaa137713c442484c890f
l1irik-rbk <kir.baranow2010@gmail.com>
2021-12-03 16:11:52 +0300

    init: start task

---
+++ migration-newip-to-ts/src/components/controller/appLoader.js
@@ -0,0 +1,11 @@
+import Loader from './loader';
+
+class AppLoader extends Loader {
+    constructor() {
+        super('https://newsapi.org/v2/', {
+            apiKey: 'd56d03f56d5443008078ee0ee3df5c67', 
+        });
+    }
+}
+
+export default AppLoader;
