e3c912827b9182b75685056332b5b60e7f517dbc
l1irik-rbk <kir.baranow2010@gmail.com>
2021-12-04 17:30:54 +0300

    feat: add appView.ts

--- migration-newip-to-ts/src/components/view/appView.ts
+++ migration-newip-to-ts/src/components/view/appView.ts
@@ -1,18 +1,54 @@
 import News from './news/news';
 import Sources from './sources/sources';
 
+interface IDataArticles {
+    status: string;
+    totalResults: number;
+    articles: Array<IArticles>
+}
+
+interface IArticles {
+    author: string;
+    content: string;
+    description: string;
+    publishedAt: string;
+    title: string;
+    url: string;
+    urlToImage: string;
+    source: { id: string, name: string }
+}
+
+interface IDataSources {
+    status: string;
+    sources: Array<ISourses>;
+}
+
+interface ISourses {
+    author: string;
+    content: string;
+    description: string;
+    publishedAt: string;
+    title: string;
+    url: string;
+    urlToImage: string;
+    source: { id: string, name: string }
+}
+
 export class AppView {
+    news: News;
+    sources: Sources;
+
     constructor() {
         this.news = new News();
         this.sources = new Sources();
     }
 
-    drawNews(data) {
+    drawNews(data: IDataArticles) {
         const values = data?.articles ? data?.articles : [];
         this.news.draw(values);
     }
 
-    drawSources(data) {
+    drawSources(data: IDataSources) {
         const values = data?.sources ? data?.sources : [];
         this.sources.draw(values);
     }
