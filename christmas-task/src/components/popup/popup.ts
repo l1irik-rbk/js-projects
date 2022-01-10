89d6147e297fcc4cd43f2a368592d8971c577352
l1irik-rbk <kir.baranow2010@gmail.com>
2022-01-10 13:54:06 +0300

    fix: popup errors

--- christmas-task/src/components/popup/popup.ts
+++ christmas-task/src/components/popup/popup.ts
@@ -1,11 +1,11 @@
-import { popupAnswer, popupText } from "../helpers/constants"
-import './popup.css'
+import { popupAnswer, popupText } from '../helpers/constants';
+import './popup.css';
 
-export function showPopup(text) {
-  popupAnswer.style.display = 'block'
-  popupText.textContent = text
+export function showPopup(text: string) {
+  popupAnswer.style.display = 'block';
+  popupText.textContent = text;
 }
 
 export function closePopup() {
-  popupAnswer.style.display = 'none'
+  popupAnswer.style.display = 'none';
 }
